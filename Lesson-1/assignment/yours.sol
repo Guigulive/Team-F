8e3e7032d494d7e2148e2c9eee40f3043d36a281
shihy52x <shihongbo1234@gmail.com>
2018-03-13 13:50:35 -0700

    52_史红波_第一节课作业
    昨天好像没有提交成功

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,45 @@
 /*作业请提交在这个目录下*/
+/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll{
+    uint salary = 1 wei;
+    address frank = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+    uint constant payDuration = 10 seconds;
+    uint lastPayday = now;
+    
+    function addFund() payable returns(uint){
+        return this.balance;
+    }
+    
+    function calculateRunway() returns(uint){
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns(bool){
+        return calculateRunway() > 0 ;
+        
+    }
+    
+    function  getPaid(){
+        if(msg.sender != frank){
+            revert();
+        }
+        uint nextPayday = lastPayday + payDuration;
+        if(nextPayday > now){
+            revert();
+        }
+        lastPayday = nextPayday;
+        frank.transfer(salary);
+    }
+    
+    function getSalary(uint x) returns(uint){
+        salary = x;
+        return salary;
+    }
+    
+    function getAddress(address x){
+        frank = x;
+    }
+    
+}
