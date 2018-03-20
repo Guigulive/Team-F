441ac061a7a80458f9b490d0bbc84fbcf4e97337
Ji Yang <yangji1993@hotmail.com>
2018-03-19 20:49:40 -0700

    第三次作业

--- Lesson-3/assignment/yours.sol
+++ Lesson-3/assignment/yours.sol
@@ -1,1 +1,171 @@
 /*作业请提交在这个目录下*/
+
+=========================================================================================
+第一题：完成今天所开发的合约产品化内容，使用Remix调用每一个函数，提交函数调用截图                    |
+=========================================================================================
+
+https://github.com/yyssjj33/Team-F/raw/53-%E6%9D%A8%E6%A5%AB/Lesson-3/assignment/%E4%BD%9C%E4%B8%9A%E4%B8%89%E7%AC%AC%E4%B8%80%E9%A2%98%E6%88%AA%E5%9B%BE.zip
+
+
+
+=========================================================================================
+第二题：增加 changePaymentAddress 函数，更改员工的薪水支付地址，思考一下能否使用modifier整合某个功能|
+=========================================================================================
+可以整合partialPaid函数到modifier
+modifier partialPaid(employeeId) {
+    _partialPaid(employees[employeeId]);
+    _;
+}
+    
+function changePaymentAddress(address oldAddress, address newAddress) onlyOwner employeeExist(oldAddress) partialPaid(oldAddress) {
+    var employee = employees[oldAddress];
+    var newEmployee = Employee(newAddress, employee.salary * 1 ether, now);
+    delete employees[oldAddress];
+    employees[newAddress] = newEmployee;
+}
+
+完整代码
+---------------------------------------------------------------------
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    
+    struct Employee {
+        address id;
+        uint salary;
+        uint lastPayday;
+    }
+
+    address thisAddress = this;
+    uint constant payDuration = 10 seconds;
+    
+    address owner;
+    mapping(address => Employee) public employees;
+    
+    uint totalSalary;
+    
+    function Payroll() public {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    modifier employeeExist(address employeeId) {
+        var employee = employees[employeeId];
+        assert(employee.id != 0x0);
+        _;
+    }
+    
+    modifier partialPaid(address employeeId) {
+        _partialPaid(employees[employeeId]);
+        _;
+    }
+    
+    function _partialPaid(Employee employee) private {
+        if (employee.id != 0x0) {
+            employee.id.transfer((now-employee.lastPayday)*employee.salary/payDuration);
+        }
+    }
+    
+    function addEmployee(address employeeId, uint salary) onlyOwner {
+        var employee = employees[employeeId];
+        assert(employee.id == 0x0);
+        employees[employeeId] = Employee(employeeId, salary * 1 ether, now);
+        totalSalary += salary * 1 ether;
+    }
+
+    function removeEmployee(address employeeId) onlyOwner employeeExist(employeeId) partialPaid(employeeId){
+        var employee = employees[employeeId];
+        totalSalary -= employee.salary;
+        delete employees[employeeId];
+    }
+    
+    function updateEmployee(address employeeId, uint s) onlyOwner employeeExist(employeeId) partialPaid(employeeId){
+        var employee = employees[employeeId];
+        
+        totalSalary -= employee.salary;
+        totalSalary += s * 1 ether;
+        employees[employeeId].salary = s * 1 ether;
+        employees[employeeId].lastPayday = now;
+    }
+    
+    function changePaymentAddress(address oldAddress, address newAddress) onlyOwner employeeExist(oldAddress) partialPaid(oldAddress){
+        var employee = employees[oldAddress];
+        var newEmployee = Employee(newAddress, employee.salary * 1 ether, now);
+        delete employees[oldAddress];
+        employees[newAddress] = newEmployee;
+        
+    }
+    
+    function addFund() payable public returns (uint)  {
+        return thisAddress.balance;
+    }
+    
+    function calculateRunway() public view returns (uint) {
+        return thisAddress.balance / totalSalary;
+    }
+    
+    function hasEnoughFund() public view returns (bool) {
+        return calculateRunway() > 0;
+    }
+    
+    function getPaid() employeeExist(msg.sender){
+        var employee = employees[msg.sender];
+        
+        uint nextPayday = employee.lastPayday + payDuration;
+        assert(nextPayday < now);
+        
+        employee.lastPayday = nextPayday;
+        employee.id.transfer(employee.salary);
+    }    
+}
+---------------------------------------------------------------------
+
+
+
+=========================================================================================
+第三题（加分题）：自学C3 Linearization, 求以下 contract Z 的继承线                            |
+contract O                                                                              |
+contract A is O                                                                         |
+contract B is O                                                                         |
+contract C is O                                                                         |
+contract K1 is A, B                                                                     |
+contract K2 is A, C                                                                     |
+contract Z is K1, K2                                                                    |
+=========================================================================================
+    O
+ /  |  \
+A   B   C
+| \ /   |
+| / \   | 
+ k1   k2
+   \  /
+    Z
+    
+    
+L[O] = O
+L[C] = C + merge(L[O], O) = C + merge(O, O) = CO
+L[B] = B + merge(L[O], O) = B + merge(O, O) = BO
+L[A] = A + merge(L[O], O) = A + merge(O, O) = AO
+
+L[K2] = K2 + merge(L[C], L[B], CB)
+      = K2 + merge(CO, BO, CB)
+      = K2 + C + merge(O, BO, B)
+      = K2 + C + B + mergeO, O)
+      = K2CBO
+同理
+L[K1] = K1CAO
+
+L[Z] = Z + merge(L[K2], L[K1], K2K1)
+     = Z + merge(K2CBO, K1BAO, K2K1)
+     = Z + K2 + merge(CBO, K1BAO, K1)
+     = Z + K2 + K1 + merge(CBO, CAO)
+     = Z + K2 + K1 + C + merge(BO, AO)
+     = Z + K2 + K1 + C + B + merge(O, AO)
+     = Z + K2 + K1 + C + B + A + merge(O, O)
+     = Z + K2 + K1 + C + B + A + O
+     = ZK2K1CBAO
+L[Z] = ZK2K1CBAO
