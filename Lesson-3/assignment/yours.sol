657d46d73e1c947239511ee0a8af2c5ba7ccaa67
shihy52x <shihongbo1234@gmail.com>
2018-03-21 08:05:43 -0700

    Update yours.sol

--- Lesson-3/assignment/yours.sol
+++ Lesson-3/assignment/yours.sol
@@ -1,1 +1,87 @@
 /*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    struct Employee {
+        address id;
+        uint salary;
+        uint lastPayday;
+    }
+    
+    uint constant payDuration = 10 seconds;
+    uint totalSalary = 0;
+    address owner;
+    mapping (address=>Employee) employees;
+    
+    function Payroll() {
+        owner = msg.sender;
+    }
+    
+    function _partialPaid(Employee employee) private {
+        if(employee.id !=0x0){
+            uint payment = employee.salary * (now - employee.lastPayday) / payDuration;
+            employee.id.transfer(payment);
+        }
+    }
+    
+
+
+    function addEmployee(address employeeId, uint salary) {
+        require(msg.sender == owner);
+        var employee  = employees[employeeId];
+        assert(employee.id == 0x0);
+        employees[employeeId] = Employee(employeeId, salary * 1 ether,now);
+        totalSalary += salary * 1 ether;
+    }
+    
+    function removeEmployee(address employeeId) {
+        require(msg.sender == owner);
+        var employee = employees[employeeId];
+        assert(employee.id != 0x0);
+        _partialPaid(employee);
+        totalSalary -= employee.salary * 1 ether;
+
+        delete employees[employeeId];
+
+    }
+    
+    function updateEmployee(address employeeId, uint salary) {
+        require(msg.sender == owner);
+        var employee = employees[employeeId];
+        assert(employee.id != 0x0);
+        employees[employeeId].lastPayday = now;
+        _partialPaid(employee);
+        totalSalary += (salary - employee.salary) * 1 ether;
+        employees[employeeId].salary = salary * 1 ether;
+
+        
+    }
+    
+    function checkEmployee(address employeeId) returns(uint salary, uint lastPayday) {
+        var employee = employees[employeeId];
+        salary = employee.salary;
+        lastPayday = employee.lastPayday;
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    function calculateRunway() returns (uint) {
+        return this.balance / totalSalary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return this.calculateRunway()>0;
+    }
+    
+    function getPaid() {
+        var employee = employees[msg.sender];
+        assert(employee.id != 0x0);
+        assert(now - employee.lastPayday > payDuration);
+        uint nextPayday = employee.lastPayday + payDuration;
+        employees[msg.sender].lastPayday = nextPayday;
+        employee.id.transfer(employee.salary);
+        
+    }
+}
