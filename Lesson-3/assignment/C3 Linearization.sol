116b1ca121a1cd91fe2dab0ecb2c1a1584fb8989
HuaiDa <1554417228@qq.com>
2018-03-21 18:54:57 +0800

    Update C3 Linearization.sol

--- Lesson-3/assignment/C3 Linearization.sol
+++ Lesson-3/assignment/C3 Linearization.sol
@@ -12,8 +12,8 @@ https://blog.tedxiong.com/C3_Linearization.html
 L[O] = [O]
 L[A] = [A,O]
 L[B] = [B,O]
-L[C] = [C,0]
-L[k1] = [K1]+merge(L[A],L[B],[A,B])  // merge规则：按照继承顺序加入父类，最后是一个所有父类添加顺序的序列
+L[C] = [C,O]
+L[k1] = [K1]+merge(L[A],L[B],[A,B])  // merge规则：按照继承顺序加入merge，最后再加一个所有父类顺序添加的序列
       = [K1]+merge([A,O],[B,O],[A,B]) // 规则：取一个元素（它是其他序列中的第一个元素，或不在其他序列出现,如果有多个元素符合，选前面一个）加到最前序列后面
       = [K1,A]+merge([O],[B,O],[B])
       = [K1,A,B,O]
