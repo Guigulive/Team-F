654e3f98491e496e3ddd8a695f0dd7502acb31ae
HuaiDa <1554417228@qq.com>
2018-03-26 01:27:57 +0800

    之前理解错误
    solidity的继承链是靠右的更近，这点和python不一样

--- Lesson-3/assignment/C3 Linearization.sol
+++ Lesson-3/assignment/C3 Linearization.sol
@@ -9,15 +9,17 @@ contract Z is K1, K2
 https://en.wikipedia.org/wiki/C3_linearization
 https://blog.tedxiong.com/C3_Linearization.html
 
+solidity的继承链是靠右的更近，这点和python不一样
+
 L[O] = [O]
 L[A] = [A,O]
 L[B] = [B,O]
 L[C] = [C,O]
-L[k1] = [K1]+merge(L[A],L[B],[A,B])  // merge规则：按照继承顺序加入merge，最后再加一个所有父类顺序添加的序列
-      = [K1]+merge([A,O],[B,O],[A,B]) // 规则：取一个元素（它是其他序列中的第一个元素，或不在其他序列出现,如果有多个元素符合，选前面一个）加到最前序列后面
-      = [K1,A]+merge([O],[B,O],[B])
-      = [K1,A,B,O]
-L[K2] = [k2,A,C,O]
-L[Z]  = [Z] + merge(L[K1],L[K2],[K1,K2])
-      = [Z] + merge([K1,A,B,O],[k2,A,C,O],[K1,K2])
-      = [Z,K1,K2,A,B,C,O]
+L[k1] = [K1]+merge(L[B],L[A],[B,A])  // merge规则：按照继承顺序加入merge，最后再加一个所有父类顺序添加的序列
+      = [K1]+merge([B,O],[A,O],[B,A]) // 规则：取一个元素（它是其他序列中的第一个元素，或不在其他序列出现,如果有多个元素符合，选前面一个）加到最前序列后面
+      = [K1,B]+merge([O],[A,O],[A])
+      = [K1,B,A,O]
+L[K2] = [k2,C,A,O]
+L[Z]  = [Z] + merge(L[K2],L[K1],[K2,K1])
+      = [Z] + merge([K2,C,A,O],[k1,B,A,O],[K2,K1])
+      = [Z,K2,C,K1,B,A,O]
