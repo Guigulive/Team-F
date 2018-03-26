ebbec7f0614f4b74be870861be7f34b403a447ff
shihy52x <shihongbo1234@gmail.com>
2018-03-25 17:41:01 -0700

    史红波-55-第四次作业-昨天没提交对version，这个是正确的版本

--- Lesson-4/assignment/yours.sol
+++ Lesson-4/assignment/yours.sol
@@ -7,40 +7,51 @@
 - 在test文件夹中，写出对如下两个函数的单元测试：
 - function addEmployee(address employeeId, uint salary) onlyOwner
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-
-contract('SimpleStorage', function(accounts) {
-
-  it("...should store the value 89.", function() {
-    return SimpleStorage.deployed().then(function(instance) {
-      simpleStorageInstance = instance;
-
-      return simpleStorageInstance.addEmployee(accounts[0],1000);
-    }).then(function(){
-      SimpleStorage.getTotalSalary 
-    })(function(totalSalary) {
-      assert.equal(totalSalary, 89, "The value 89 was not stored.");
-    });
-  });
-
-});
-- function removeEmployee(address employeeId) onlyOwner employeeExist(employeeId)
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
 contract('SimpleStorage', function(accounts) {
-
-  it("...should store the value 89.", function() {
+var totalSalary;
+var simpleStorageInstance;
+var salary0 =3;
+var salary1 =6;
+  it("...should store the new address.", function() {
     return SimpleStorage.deployed().then(function(instance) {
       simpleStorageInstance = instance;
-
-      return simpleStorageInstance.addEmployee(accounts[0],1000);
+      return simpleStorageInstance.addEmployee(accounts[0],salary0);
+    }).then(function() {
+      return simpleStorageInstance.addEmployee(accounts[1],salary1);
     }).then(function(){
-      SimpleStorage.getTotalSalary 
-    })(function(totalSalary) {
-      assert.equal(totalSalary, 89, "The value 89 was not stored.");
-    });
+      return simpleStorageInstance.employees(accounts[1]);
+    }).then(function(employee1){
+      address1 = employee1[0];
+      console.log("TotalSalary is");          
+      console.log(address1);  
+      console.log(accounts[1]);            
+      assert.equal(address1, accounts[1], "The new address is not added.");
+    })
+  });
+
+  it("...should remove  current address.", function() {
+    return SimpleStorage.deployed().then(function(instance) {
+      simpleStorageInstance = instance;
+      return simpleStorageInstance.addEmployee(accounts[3],salary0);
+    }).then(function() {
+      return simpleStorageInstance.addEmployee(accounts[4],salary1);
+    }).then(function(){
+      return simpleStorageInstance.removeEmployee(accounts[1])
+    }).then(function(){
+      return simpleStorageInstance.employees(accounts[1]);
+    }).then(function(employee1){
+      address1 = employee1[0];
+      console.log("TotalSalary is");          
+      console.log(address1);  
+      console.log(accounts[1]);            
+      assert.equal(address1, 0x0, "This address is not removed.");
+    })
   });
 
 });
+
 - 思考一下我们如何能覆盖所有的测试路径，包括函数异常的捕捉
 - (加分题,选作）
 - 写出对以下函数的基于solidity或javascript的单元测试 function getPaid() employeeExist(msg.sender)
